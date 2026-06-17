70e36b0a93a554a24b6e05483d140a820b71d2e7
Moises <moisesrangel97@gmail.com>
2026-06-17 08:49:08 -0300

    Operadores de Atribuicao

--- 1º Semestre/SuperTrunfoC/HoraDeCodar1/Nivel2Aventureiro/OperadoresMatematicos.c
+++ 1º Semestre/SuperTrunfoC/HoraDeCodar1/Nivel2Aventureiro/OperadoresMatematicos.c
@@ -1,29 +1,6 @@
 #include <stdio.h>
 
 int main (){
-    /*int numero1, numero2, resultado;
-
-    printf("Digite o primeiro número: ");
-    scanf("%d", &numero1);
-
-    printf("Digite o segundo número: ");
-    scanf("%d", &numero2);
-
-    resultado = numero1 + numero2;
-    printf("\nA soma dos números é: %d\n", resultado);
-
-    resultado = numero1 - numero2;
-    printf("\nA subtração dos números é: %d\n", resultado);
-
-    resultado = numero1 * numero2;
-    printf("\nA multiplicação dos números é: %d\n", resultado);
-
-    if (numero2 != 0) {
-        resultado = numero1 / numero2;
-        printf("\nA divisão dos números é: %d\n", resultado);
-    } else {
-        printf("\nNão é possível dividir por zero.\n");
-    }*/
     
     int numero1, numero2, resultado;
 
